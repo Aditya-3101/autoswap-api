bab133aaceb82b10d21243fefe9fceacb5973b8c
Aditya-3101 <adityadhayfule3101@gmail.com>
2025-02-08 19:13:27 +0530

    "migrated from json server to express api"

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080; //  choose port from here like 8080, 3001
+const data = require('./db.json')
+const cors = require('cors')
 
-server.use(middlewares);
-server.use(router);
+app.use(cors())
 
-server.listen(port);
+const {cars,allCars} = data
+
+app.use('/allCars',(req,res)=>{
+    res.json(allCars)
+})
+
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`)
+});
